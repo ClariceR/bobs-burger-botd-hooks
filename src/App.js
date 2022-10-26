844f41cb233f407df3dcfdd9c2f5bc8bd2f1cb26
clarice_r <claricerdallorto@gmail.com>
2022-10-26 21:04:22 +0100

    Calls api and displays burger of the day according to user input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import { useRef, useState, useReducer } from 'react';
+import { useRef, useReducer, useState, useEffect } from 'react';
 import './App.css';
 
-function BurgerOfTheDay() {
+function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   const inputEpisode = useRef();
-  // const [episode, setEpisode] = useState(null)
-  const [episode, dispatch] = useReducer((state, action) => {
-    return {
-      episode: action.episode,
-    };
-  }, {episode: "1"});
+  const [episode, dispatch] = useReducer(
+    (state, action) => {
+      return {
+        id: action.id,
+        name: action.name,
+      };
+    },
+    { id: '1' }
+  );
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setEpisode(inputEpisode.current.value);
-    dispatch({
-      episode: inputEpisode.current.value,
-    });
+    if (inputEpisode.current.value > 0 && inputEpisode.current.value < 334) {
+      dispatch({
+        id: inputEpisode.current.value,
+        name: data.name,
+      });
+    }
     inputEpisode.current.value = '';
   };
+
+  const URL = `https://bobsburgers-api.herokuapp.com/burgerOfTheDay/${episode.id}`;
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(URL)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [episode.id]);
+
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error)}</pre>;
+  if (!data) return null;
+
   return (
-    <>
-      <p>{episode.episode}</p>
+    <div className="App">
+      <h1>Bob's Burger</h1>
+      <h3>Burger of the day</h3>
+      <div className="board">
+        <p>{data.name}</p>
+        <p>{data.price}</p>
+      </div>
       <form onSubmit={handleSubmit}>
         <input
           ref={inputEpisode}
@@ -28,17 +58,6 @@ function BurgerOfTheDay() {
           placeholder="episode number 1 - 333"
         />
       </form>
-    </>
-  );
-}
-
-function App() {
-
-  return (
-    <div className="App">
-      <h1>Bob's Burger</h1>
-      <h3>Burger of the day</h3>
-      <BurgerOfTheDay />
     </div>
   );
 }
